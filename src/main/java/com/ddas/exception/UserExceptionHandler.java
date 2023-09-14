d15676b0f9b544a74083000e88b50f9629131cae
fxkfjla <kacper.skr11@gmail.com>
2023-09-14 12:57:07 +0200

    use api error utils

--- src/main/java/com/ddas/exception/UserExceptionHandler.java
+++ src/main/java/com/ddas/exception/UserExceptionHandler.java
@@ -1,14 +1,12 @@
 package com.ddas.exception;
 
-import java.time.LocalDateTime;
-
-import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.ddas.model.exception.ApiError;
+import com.ddas.model.exception.ApiErrorUtils;
 import com.ddas.model.exception.UserNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -17,15 +15,8 @@ import jakarta.servlet.http.HttpServletRequest;
 public class UserExceptionHandler
 {
     @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<Object> handleUserNotFoundException(UserNotFoundException e, HttpServletRequest req)
+    public ResponseEntity<ApiError> handleUserNotFoundException(UserNotFoundException e, HttpServletRequest req)
     {
-        HttpStatus status = HttpStatus.NOT_FOUND;
-
-        return new ResponseEntity<>(generateApiError(e, status.value(), req.getRequestURI()), HttpStatus.NOT_FOUND);
-    }
-
-    private Object generateApiError(Exception e, int statusCode, String reqPath)
-    {
-        return new ApiError(e.getMessage(), statusCode, LocalDateTime.now().toString(), reqPath, ExceptionUtils.getStackTrace(e));
+        return ApiErrorUtils.buildErrorResponse(e, HttpStatus.NOT_FOUND, req);
     }
 }
