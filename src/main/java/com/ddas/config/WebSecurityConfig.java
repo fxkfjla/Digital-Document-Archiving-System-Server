701cea9988c7656c8de7ba4f40c0368ecbbeb3a6
fxkfjla <kacper.skr11@gmail.com>
2023-09-16 19:48:13 +0200

    use jwt auth filter

--- src/main/java/com/ddas/config/WebSecurityConfig.java
+++ src/main/java/com/ddas/config/WebSecurityConfig.java
@@ -2,22 +2,24 @@ package com.ddas.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import com.ddas.service.UserService;
+import com.ddas.security.JwtAuthFilter;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig
 {
-    public WebSecurityConfig(UserService userService)
+    public WebSecurityConfig(AuthenticationProvider authProvider, JwtAuthFilter jwtAuthFilter)
     {
-        this.userService = userService;
+        this.authProvider = authProvider;
+        this.jwtAuthFilter = jwtAuthFilter;
     }
 
     @Bean
@@ -35,16 +37,13 @@ public class WebSecurityConfig
         .sessionManagement(session ->
             session
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        );
+        )
+        .authenticationProvider(authProvider)
+        .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     } 
 
-    @Bean
-    public UserDetailsService userDetailsService()
-    {
-        return username -> userService.findByEmail(username);
-    }
-
-    private final UserService userService;
+    private final AuthenticationProvider authProvider;
+    private final JwtAuthFilter jwtAuthFilter;
 }
