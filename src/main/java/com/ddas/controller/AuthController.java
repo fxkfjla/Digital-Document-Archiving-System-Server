05aa50b6a69b95aab0c9f43072cc992dedb0b3b6
fxkfjla <kacper.skr11@gmail.com>
2023-11-08 20:28:09 +0100

    change parameters to request body in post methods

--- src/main/java/com/ddas/controller/AuthController.java
+++ src/main/java/com/ddas/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.ddas.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,21 +21,21 @@ import lombok.AllArgsConstructor;
 public class AuthController
 {
     @PostMapping("/register")
-    public ResponseEntity<ApiResponse<String>> register(@Valid RegisterRequest user)
+    public ResponseEntity<ApiResponse<String>> register(@Valid @RequestBody RegisterRequest user)
     {
         authService.register(user);
         return ApiResponse.success("User registered successfully!");
     }
 
     @PostMapping("/login")
-    public ResponseEntity<ApiResponse<String>> login(@Valid LoginRequest user)
+    public ResponseEntity<ApiResponse<String>> login(@Valid @RequestBody LoginRequest user)
     {
         return ApiResponse.success(authService.login(user));
     }
 
     // Requestheader
     @PostMapping("/logout")
-    public ResponseEntity<ApiResponse<String>> logout(@Valid TokenRequest token)
+    public ResponseEntity<ApiResponse<String>> logout(@Valid @RequestBody TokenRequest token)
     {
         authService.logout(token);
         return ApiResponse.success("Token: " + token.token() + " invalidated!");
